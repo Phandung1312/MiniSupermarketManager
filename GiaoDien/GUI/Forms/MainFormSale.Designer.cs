1e2737189129abc2cafbd4d9e75cbce18d1eaccf
Dung Phan Thanh <power13122002@gmail.com>
2022-05-01 10:32:45 +0700

    Đăng nhập

--- GiaoDien/GUI/Forms/MainFormSale.Designer.cs
+++ GiaoDien/GUI/Forms/MainFormSale.Designer.cs
@@ -64,9 +64,9 @@
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(293, 970);
+            this.panel1.Size = new System.Drawing.Size(220, 788);
             this.panel1.TabIndex = 0;
             // 
             // button10
@@ -78,10 +78,10 @@
             this.button10.ForeColor = System.Drawing.SystemColors.Control;
             this.button10.Image = global::GiaoDien.Properties.Resources.account_skin_type_1_48px;
             this.button10.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button10.Location = new System.Drawing.Point(0, 331);
-            this.button10.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button10.Location = new System.Drawing.Point(0, 268);
+            this.button10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(293, 62);
+            this.button10.Size = new System.Drawing.Size(220, 50);
             this.button10.TabIndex = 39;
             this.button10.Text = "Tài khoản cá nhân   ";
             this.button10.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -96,10 +96,10 @@
             this.buttonSchdule.ForeColor = System.Drawing.SystemColors.Control;
             this.buttonSchdule.Image = global::GiaoDien.Properties.Resources.Tear_Off_Calendar_48px;
             this.buttonSchdule.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonSchdule.Location = new System.Drawing.Point(0, 269);
-            this.buttonSchdule.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonSchdule.Location = new System.Drawing.Point(0, 218);
+            this.buttonSchdule.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonSchdule.Name = "buttonSchdule";
-            this.buttonSchdule.Size = new System.Drawing.Size(293, 62);
+            this.buttonSchdule.Size = new System.Drawing.Size(220, 50);
             this.buttonSchdule.TabIndex = 38;
             this.buttonSchdule.Text = "Lịch Làm Việc         ";
             this.buttonSchdule.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -115,10 +115,10 @@
             this.button9.ForeColor = System.Drawing.SystemColors.Control;
             this.button9.Image = global::GiaoDien.Properties.Resources.trolley_48px;
             this.button9.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button9.Location = new System.Drawing.Point(0, 207);
-            this.button9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button9.Location = new System.Drawing.Point(0, 168);
+            this.button9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(293, 62);
+            this.button9.Size = new System.Drawing.Size(220, 50);
             this.button9.TabIndex = 37;
             this.button9.Text = "Kệ Hàng                    ";
             this.button9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -133,10 +133,10 @@
             this.button3.ForeColor = System.Drawing.SystemColors.Control;
             this.button3.Image = global::GiaoDien.Properties.Resources.bill_48px;
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button3.Location = new System.Drawing.Point(0, 145);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(0, 118);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(293, 62);
+            this.button3.Size = new System.Drawing.Size(220, 50);
             this.button3.TabIndex = 31;
             this.button3.Text = "Hóa Đơn                   ";
             this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -145,10 +145,9 @@
             // panel4
             // 
             this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel4.Location = new System.Drawing.Point(0, 113);
-            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel4.Location = new System.Drawing.Point(0, 92);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(293, 32);
+            this.panel4.Size = new System.Drawing.Size(220, 26);
             this.panel4.TabIndex = 5;
             // 
             // panel3
@@ -156,18 +155,17 @@
             this.panel3.Controls.Add(this.pictureBox1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(293, 113);
+            this.panel3.Size = new System.Drawing.Size(220, 92);
             this.panel3.TabIndex = 4;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::GiaoDien.Properties.Resources.red_shopping_basket_100px;
-            this.pictureBox1.Location = new System.Drawing.Point(75, 5);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(56, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(127, 106);
+            this.pictureBox1.Size = new System.Drawing.Size(95, 86);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -181,18 +179,19 @@
             this.panel2.Controls.Add(this.labelUserName);
             this.panel2.Controls.Add(this.pictureUser);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(293, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Location = new System.Drawing.Point(220, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1555, 64);
+            this.panel2.Size = new System.Drawing.Size(1166, 52);
             this.panel2.TabIndex = 6;
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(1363, -1);
+            this.label2.Location = new System.Drawing.Point(1022, -1);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(99, 64);
+            this.label2.Size = new System.Drawing.Size(74, 52);
             this.label2.TabIndex = 7;
             this.label2.Text = "Đăng xuất";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -201,10 +200,9 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Lavender;
             this.button1.Image = global::GiaoDien.Properties.Resources.Logout_48px;
-            this.button1.Location = new System.Drawing.Point(1468, 0);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(1101, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(71, 64);
+            this.button1.Size = new System.Drawing.Size(53, 52);
             this.button1.TabIndex = 6;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -214,19 +212,19 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Freestyle Script", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(493, 5);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(370, 4);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(461, 52);
+            this.label1.Size = new System.Drawing.Size(379, 42);
             this.label1.TabIndex = 5;
             this.label1.Text = "Mini Supermarket System Manager";
             // 
             // labelUserName
             // 
             this.labelUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelUserName.Location = new System.Drawing.Point(80, 18);
+            this.labelUserName.Location = new System.Drawing.Point(60, 15);
+            this.labelUserName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelUserName.Name = "labelUserName";
-            this.labelUserName.Size = new System.Drawing.Size(168, 27);
+            this.labelUserName.Size = new System.Drawing.Size(126, 22);
             this.labelUserName.TabIndex = 4;
             this.labelUserName.Text = "Nhân viên";
             this.labelUserName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -234,10 +232,10 @@
             // pictureUser
             // 
             this.pictureUser.Image = global::GiaoDien.Properties.Resources.cristiano_ronaldo_96px1;
-            this.pictureUser.Location = new System.Drawing.Point(5, 5);
-            this.pictureUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureUser.Location = new System.Drawing.Point(4, 4);
+            this.pictureUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureUser.Name = "pictureUser";
-            this.pictureUser.Size = new System.Drawing.Size(69, 57);
+            this.pictureUser.Size = new System.Drawing.Size(52, 46);
             this.pictureUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureUser.TabIndex = 3;
             this.pictureUser.TabStop = false;
@@ -248,10 +246,10 @@
             this.panelDesktop.Controls.Add(this.label4);
             this.panelDesktop.Controls.Add(this.label3);
             this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelDesktop.Location = new System.Drawing.Point(293, 64);
-            this.panelDesktop.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelDesktop.Location = new System.Drawing.Point(220, 52);
+            this.panelDesktop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panelDesktop.Name = "panelDesktop";
-            this.panelDesktop.Size = new System.Drawing.Size(1555, 906);
+            this.panelDesktop.Size = new System.Drawing.Size(1166, 736);
             this.panelDesktop.TabIndex = 7;
             // 
             // label4
@@ -259,10 +257,9 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Blue;
-            this.label4.Location = new System.Drawing.Point(651, 215);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(488, 175);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(152, 54);
+            this.label4.Size = new System.Drawing.Size(121, 42);
             this.label4.TabIndex = 1;
             this.label4.Text = "Name";
             // 
@@ -271,29 +268,29 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.label3.Location = new System.Drawing.Point(620, 124);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(465, 101);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(222, 54);
+            this.label3.Size = new System.Drawing.Size(180, 42);
             this.label3.TabIndex = 0;
             this.label3.Text = "Xin Chào";
             // 
             // MainFormSale
             // 
             this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(1848, 970);
+            this.ClientSize = new System.Drawing.Size(1386, 788);
             this.Controls.Add(this.panelDesktop);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainFormSale";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
